253661b250a19905cc44dd66c935eae587275f72
Cassiel88 <krapivcenkon@gmail.com>
2023-04-04 07:13:40 +0300

    add Lesson

---
+++ Lessom_17.7.3.py
@@ -0,0 +1,8 @@
+per_cent = {'ТБК': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = int(input('Пожалуйста, введите сумму вклада:'))
+deposit = []
+for key in per_cent: deposit.append(int(per_cent[key] * money / 100))
+max_deposit = max(deposit)
+print(deposit)
+print('Максимальная сумма, которую Вы можете заработать - ' + str(max_deposit))
+

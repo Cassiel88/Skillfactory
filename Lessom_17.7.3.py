2d32bd8be9a20fc0b5e08024cd9b2fc62ae8878c
Cassiel88 <125628751+Cassiel88@users.noreply.github.com>
2023-04-05 06:20:09 +0300

    Update Lessom_17.7.3.py

--- Lessom_17.7.3.py
+++ Lessom_17.7.3.py
@@ -1,5 +1,5 @@
 per_cent = {'ТБК': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-money = int(input('Пожалуйста, введите сумму вклада:'))
+money = int(input('Пожалуйста, введите сумму депозита:'))
 deposit = []
 for key in per_cent: deposit.append(int(per_cent[key] * money / 100))
 max_deposit = max(deposit)
